b14b6ecaf1b9332186d97adec7c11adcf3172315
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-03-08 19:11:34 +0000

    Add heaer file
    Add header, and create nodes function.

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node.
+ * @parent: pointer to the parent node of the node to create.
+ * @value: value to put in the new node.
+ * Return: a pointer to the new node.
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+}
