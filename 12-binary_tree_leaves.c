7067bfacf118e7e285cdbbfa373e34b4c3ad7c70
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-03-09 20:17:15 +0000

    Add function to return numbers of leaves of a BT

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree;
+ * @tree: pointer to the root node of the tree to count the number of leaves.
+ * Return: Number of leaves of binary tree or 0 if tree is NULL.
+*/
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t n_leaves = 0;
+
+	if (!tree)
+		return (0);
+	n_leaves += binary_tree_leaves(tree->left);
+	n_leaves += binary_tree_leaves(tree->right);
+	if (tree->parent && tree->left == NULL && tree->right == NULL)
+		n_leaves += 1;
+	return (n_leaves);
+}
