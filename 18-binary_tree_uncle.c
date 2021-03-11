7f873b931cac48860bfcd0aed17ce80c2f7eaa61
Alejandro Torres <alejo.torres1247@gmail.com>
2021-03-11 11:54:23 -0500

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,7 +8,7 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node || node->parent->parent == NULL)
+	if (!node || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
